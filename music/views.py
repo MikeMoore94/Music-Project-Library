308c5ab560a1223537e81134560ec551e841fd5e
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-11-01 14:44:52 -0400

    added new class get_by_id and moethod to get song by id, to delete song, and add song

--- music/views.py
+++ music/views.py
@@ -21,4 +21,33 @@ class SongList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class SongDetail(APIView):
+    
+    def get_by_id(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            return HttpResponse(status=404)
+        
+    def get(self, request, pk):
+        song = self.get_by_id(pk)
+        serializer = SongSerializer(song)
 
+        return Response(serializer.data)
+        
+    def put(self, request, pk):
+        song = self.get_by_id(pk)
+        serializer = SongSerializer(song, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+        
+    def delete(self, request, pk):
+        song = self.get_by_id(pk)
+        serializer = SongSerializer(song)
+
+        song.delete()
+        
+        return Response(serializer.data, status=200)
