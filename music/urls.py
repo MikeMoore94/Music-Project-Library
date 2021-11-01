f443dcb3428993e554dd22bfd7ebba5322750142
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-11-01 14:24:14 -0400

    added a song through postman, all is working

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatters = [
-    path('music/', views.SongList.as_views()),
+urlpatterns = [
+    path('music/', views.SongList.as_view()),
 ]
--- music/views.py
+++ music/views.py
@@ -3,6 +3,7 @@ from .models import Song
 from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status 
+from .serializers import SongSerializer
 # Create your views here.
 
 
