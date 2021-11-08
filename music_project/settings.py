d23210a693376e12c1ca328fa892efa06c7ad1cc
Micheal Moore <michealmoore@Micheals-MacBook-Air.local>
2021-11-08 11:37:04 -0500

    added cors to te setting.py for musci library project

--- music_project/settings.py
+++ music_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'music'
+    'music',
+    'corsheaders'
 
 ]
 
@@ -50,7 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_ALLOW_ALL=True 
 
 ROOT_URLCONF = 'music_project.urls'
 
